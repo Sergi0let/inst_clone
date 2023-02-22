856dcc262adeb04e8c9ac2813c06555db70053e7
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-22 18:13:50 +0200

    make the feed section responsive

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,10 @@
-import { DotsHorizontalIcon } from '@heroicons/react/outline';
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  BookmarkIcon,
+  EmojiHappyIcon,
+} from '@heroicons/react/outline';
 
 export default function Post({ username, userImg, img, caption }) {
   return (
@@ -16,13 +22,32 @@ export default function Post({ username, userImg, img, caption }) {
 
       {/* Post Image */}
       <img src={img} alt={caption} className="object-cover w-full" />
+
+      {/* Post buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
+      {/* Post Coments */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+
+      {/*Post Inputbox*/}
+      <form action="" className="flex items-center p-4 ">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          className="border-none flex-1 focus:ring-0"
+          type="text"
+          placeholder="Enter you comment..."
+        />
+        <button className="text-blue-400 font-bold">Post</button>
+      </form>
     </div>
   );
 }
-// <div>
-//   <img src={userImg} alt={username} />
-//   <p>{username}</p>
-// </div>
-// <div>
-//   <img src={img} alt={caption} />
-// </div>
