a7ac53884a6fa84f35acff1a0cde2f95687d56d5
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-22 16:04:09 +0200

    crate Posts

---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import { DotsHorizontalIcon } from '@heroicons/react/outline';
+
+export default function Post({ username, userImg, img, caption }) {
+  return (
+    <div className="bg-white my-7 border rounded-md ">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-20 rounded-full object-cover border p-1 mr-3 cursor-pointer"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* Post Image */}
+      <img src={img} alt={caption} className="object-cover w-full" />
+    </div>
+  );
+}
+// <div>
+//   <img src={userImg} alt={username} />
+//   <p>{username}</p>
+// </div>
+// <div>
+//   <img src={img} alt={caption} />
+// </div>
