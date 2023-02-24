e349b2487b58408511cb61a4072c71ee34b9a2f6
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-24 17:13:38 +0200

    get post data from dataBase and render it  the feed component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,38 +1,32 @@
 import Post from './Post';
+import { useState, useEffect } from 'react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '/firebase';
 
 export default function Posts() {
-  const posts = [
-    {
-      id: '1',
-      username: 'fox mounder',
-      userImg:
-        'https://www.akc.org/wp-content/uploads/2021/05/French-Bulldog-puppy-head-portrait-outdoors-500x485.jpeg',
-      img: 'https://images.unsplash.com/photo-1677011555044-b38992539a9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      caption:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    },
-    {
-      id: '2',
-      username: 'sky walker',
-      userImg:
-        'https://www.akc.org/wp-content/uploads/2021/05/French-Bulldog-puppy-head-portrait-outdoors-500x485.jpeg',
-      img: 'https://images.unsplash.com/photo-1677014807699-85d9639e957f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      caption:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+  }, []);
 
   return (
     <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
-        />
-      ))}
+      {posts &&
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            username={post.data().username}
+            userImg={post.data().profileImg}
+            img={post.data().image}
+            caption={post.data().caption}
+          />
+        ))}
     </div>
   );
 }
